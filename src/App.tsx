63fdb9ed0b5079a545bdcb95b81218beb0b4e755
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-03-14 16:19:38 +0900

    chore: Footer, Nav 파일 생성

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,19 @@ import "./assets/css/tailwind.css";
 import { BrowserRouter } from "react-router-dom";
 import Drawer from "./components/common/Drawer";
 import Router from "./router/router";
-import { RecoilRoot } from "recoil";
 
 const App = (): JSX.Element => {
   return (
     <BrowserRouter>
-      <RecoilRoot>
-        <input type="checkbox" id="side-menu" className="drawer-toggle" />
-        <section className="drawer-content">
-          {/* Nav를 렌더링 하세요 */}
-          <section className="main pt-16">
-            <Router />
-          </section>
-          {/* Footer를 렌더링 하세요 */}
+      <input type="checkbox" id="side-menu" className="drawer-toggle" />
+      <section className="drawer-content">
+        {/* Nav를 렌더링 하세요 */}
+        <section className="main pt-16">
+          <Router />
         </section>
-        <Drawer />
-      </RecoilRoot>
+        {/* Footer를 렌더링 하세요 */}
+      </section>
+      <Drawer />
     </BrowserRouter>
   );
 };
