cbd61f4f79f14b2e3e19dec94439b3cd9521c35b
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-03-14 15:01:49 +0900

    feat: recoil 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,22 @@ import "./assets/css/tailwind.css";
 import { BrowserRouter } from "react-router-dom";
 import Drawer from "./components/common/Drawer";
 import Router from "./router/router";
+import { RecoilRoot } from "recoil";
 
 const App = (): JSX.Element => {
   return (
     <BrowserRouter>
-      <input type="checkbox" id="side-menu" className="drawer-toggle" />
-      <section className="drawer-content">
-        {/* Nav를 렌더링 하세요 */}
-        <section className="main pt-16">
-          <Router />
+      <RecoilRoot>
+        <input type="checkbox" id="side-menu" className="drawer-toggle" />
+        <section className="drawer-content">
+          {/* Nav를 렌더링 하세요 */}
+          <section className="main pt-16">
+            <Router />
+          </section>
+          {/* Footer를 렌더링 하세요 */}
         </section>
-        {/* Footer를 렌더링 하세요 */}
-      </section>
-      <Drawer />
+        <Drawer />
+      </RecoilRoot>
     </BrowserRouter>
   );
 };
--- src/views/Index.tsx
+++ src/views/Index.tsx
@@ -1,17 +1,21 @@
 import Slider from "../components/common/Slider";
+import { useRecoilState } from "recoil";
+import textState from "../atom.js";
 
 const Index = (): JSX.Element => {
+  const [text, setText] = useRecoilState(textState);
+  console.log(text);
   return (
     <>
       <Slider />
       <section className="pt-6 lg:pt-12 pb-4 lg:pb-8 px-4 xl:px-2 mt-10 xl:container mx-auto">
-         {/* componetns products 폴더에 공통으로 사용할 ItemList 컴포넌트를 만들어서 노출 시켜 보세요. */}
+        {/* componetns products 폴더에 공통으로 사용할 ItemList 컴포넌트를 만들어서 노출 시켜 보세요. */}
       </section>
       <section className="pt-6 lg:pt-12 pb-4 lg:pb-8 px-4 xl:px-2 xl:container mx-auto">
-         {/* componetns products 폴더에 공통으로 사용할 ItemList 컴포넌트를 만들어서 노출 시켜 보세요. */}
+        {/* componetns products 폴더에 공통으로 사용할 ItemList 컴포넌트를 만들어서 노출 시켜 보세요. */}
       </section>
       <section className="pt-6 lg:pt-12 pb-4 lg:pb-8 px-4 xl:px-2 mb-20 xl:container mx-auto">
-         {/* componetns products 폴더에 공통으로 사용할 ItemList 컴포넌트를 만들어서 노출 시켜 보세요. */}
+        {/* componetns products 폴더에 공통으로 사용할 ItemList 컴포넌트를 만들어서 노출 시켜 보세요. */}
       </section>
     </>
   );
