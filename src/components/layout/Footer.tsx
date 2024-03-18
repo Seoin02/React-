6da5900544207868f3ed5947a14376dbd5d4ca85
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-03-18 16:22:45 +0900

    style: Footer UI 구현

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -5,11 +5,14 @@ import master from "../../assets/img/svg/master.svg";
 import paypal from "../../assets/img/svg/paypal.svg";
 import dinerClub from "../../assets/img/svg/dinersClub.svg";
 import discover from "../../assets/img/svg/discover.svg";
+import facebook from "../../assets/img/svg/facebook.svg";
+import instagram from "../../assets/img/svg/instagram.svg";
+import github from "../../assets/img/svg/github.svg";
 
 const Footer = () => {
   return (
-    <>
-      <div>
+    <footer className="p-10 footer bg-base-200 text-base-content footer-center">
+      <div className="flex gap-2">
         <img src={visa} />
         <img src={americanExpress} />
         <img src={master} />
@@ -17,9 +20,13 @@ const Footer = () => {
         <img src={dinerClub} />
         <img src={discover} />
       </div>
-      <div>SNS 로고</div>
-      <div>Copyright ©</div>
-    </>
+      <div className="grid grid-flow-col gap-4">
+        <img src={facebook} className="w-4" />
+        <img src={instagram} className="w-6" />
+        <img src={github} className="w-6" />
+      </div>
+      <div>Copyright © Seoin02</div>
+    </footer>
   );
 };
 
