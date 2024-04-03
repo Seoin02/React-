e4fec01f517751a53e12b1af814b88bc560f36fd
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-04-03 17:22:03 +0900

    feat: side-slider에 카테고리 링크 적용

--- src/components/common/Drawer.tsx
+++ src/components/common/Drawer.tsx
@@ -4,13 +4,19 @@ const Drawer = (): JSX.Element => {
       <label htmlFor="side-menu" className="drawer-overlay"></label>
       <ul className="menu w-60 sm:w-80 p-4 overflow-y-auto bg-white dark:bg-base-100">
         <li>
-          <a className="!text-gray-700 active:!text-white dark:!text-white">패션</a>
+          <a className="!text-gray-700 active:!text-white dark:!text-white" href="/fashion">
+            패션
+          </a>
         </li>
         <li>
-          <a className="!text-gray-700 active:!text-white dark:!text-white">액세서리</a>
+          <a className="!text-gray-700 active:!text-white dark:!text-white" href="/accessory">
+            액세서리
+          </a>
         </li>
         <li>
-          <a className="!text-gray-700 active:!text-white dark:!text-white">디지털</a>
+          <a className="!text-gray-700 active:!text-white dark:!text-white" href="/digital">
+            디지털
+          </a>
         </li>
       </ul>
     </div>
