7f97493dbf23f44bd72c62f3cb19fbead7e52255
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-08-04 16:28:55 +0900

    feat: store.ts 파일 생성

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
