f084baac88e5841fe1de0db208e66d1497a599d1
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-08-04 18:01:55 +0900

    remove: 불필요한 코드 제거

---
+++ src/components/cart/CartList.tsx
@@ -0,0 +1,8 @@
+import { Link } from "react-router-dom";
+import { toCurrencyFormat } from "../../utils/util";
+
+const CartList = (): JSX.Element => {
+  return <div className="lg:flex lg:items-center mt-4 px-2 lg:px-0">{/* 카트 리스트 화면을 구성 해보세요. */}</div>;
+};
+
+export default CartList;
