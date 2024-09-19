c0a99e1252667d77c60d604741833155c426a6d0
Seoin <seoin2269@naver.com>
2024-09-19 14:42:04 +0900

    refactor: 서스펜스 app 컴포넌트 전역 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
+import { Suspense } from "react";
 import { BrowserRouter } from "react-router-dom";
 import Drawer from "./components/common/Drawer";
 import Router from "./router/router";
 import Nav from "./components/layout/Nav";
 import Footer from "./components/layout/Footer";
 import "./assets/css/tailwind.css";
+import Loading from "./components/common/Loading";
 
 const App = (): JSX.Element => {
   return (
     <BrowserRouter>
-      <input type="checkbox" id="side-menu" className="drawer-toggle" />
-      <section className="drawer-content">
-        <Nav />
-        <section className="main pt-16">
-          <Router />
+      <Suspense fallback={<Loading />}>
+        <input type="checkbox" id="side-menu" className="drawer-toggle" />
+        <section className="drawer-content">
+          <Nav />
+          <section className="main pt-16">
+            <Router />
+          </section>
+          <Footer />
         </section>
-        <Footer />
-      </section>
-      <Drawer />
+        <Drawer />
+      </Suspense>
     </BrowserRouter>
   );
 };
