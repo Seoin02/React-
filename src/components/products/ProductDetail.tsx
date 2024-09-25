dabac5713d74481a99967fcf7d6756276509f02f
Seoin <seoin2269@naver.com>
2024-09-25 16:39:20 +0900

    refactor: 이미지 요소 고정값 지정해 레이아웃 시프트 개선

--- src/components/products/ProductDetail.tsx
+++ src/components/products/ProductDetail.tsx
@@ -14,7 +14,7 @@ const ProductDetail = ({ product }): JSX.Element => {
     <div key={product.id} className="lg:flex lg: gap-36 lg:items-center mt-6 md:mt-14 px-2 lg:px-0">
       <div className="lg:flex lg:items-center mt-6 md:mt-14 px-2 lg:px-0">
         <figure className="flex-shrink-0 items-center rounded-2xl overflow-hidden px-4 py-4 bg-white view_image">
-          <img className="object-contain w-56 h-full" src={product.image} alt={product.title} />
+          <img className="object-contain w-56 h-80" src={product.image} alt={product.title} />
         </figure>
       </div>
       <div className="card-body px-1 lg:px-12">
